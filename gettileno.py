8e21101a4d41621115b666741bc325361cba56cf
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-03-18 02:10:17 -0700

    It's late...

--- gettileno.py
+++ gettileno.py
@@ -1,34 +1,52 @@
 import math
 import pprint
 import time
+import os
+import urllib2
+
+def fetch_tile(tile):
+    target_filepattern = "static/images/%s"
+    filename = "%d/%d/%d.png" % (tile[0], tile[1], tile[2])
+    folder = os.path.dirname(target_filepattern % filename)
+    try:
+        os.makedirs(folder)
+    except OSError:
+        pass
+
+    if not os.path.exists(target_filepattern % filename):
+        print "Fetching %s" % filename
+        d = urllib2.urlopen('http://a.tile.osm.org/%s' % filename)
+        with open(target_filepattern % filename, 'w') as output_file:
+            output_file.write(d.read())
+        time.sleep(.1)
 
 tiles = set()
 
 # 39.836 to 40.213
 # -119.272 to -118.601
-min_latitude = 38.836
-max_latitude = 41.213
+min_latitude = 39.836
+max_latitude = 40.213
 latitude = min_latitude
 
-min_longitude = -120.272
-max_longitude = -117.601
-longitude = min_longitude
+min_longitude = -119.272
+max_longitude = -118.601
 
 while latitude <= max_latitude:
+    longitude = min_longitude
     while longitude <= max_longitude:
-        for zoom in range(5, 18, 1):
-            n = pow(zoom, 2)
+        for zoom in range(5, 15, 1):
+            n = pow(2, zoom)
             latitude_rad = math.radians(latitude)
             x = n * ((longitude + 180) / 360)
             y = n * (1 - (math.log(math.tan(latitude_rad) +
                     (1 / math.cos(latitude_rad))) / math.pi)) / 2
-            tiles.add((zoom, int(x), int(y)))
-        longitude += .1
-    latitude += .1
+            tile = (zoom, int(x), int(y))
+            if tile not in tiles:
+                fetch_tile(tile)
+                tiles.add(tile)
+        longitude += .001
+    latitude += .001
 
 pprint.pprint(tiles)
 print("%d tiles" % len(tiles))
 
-for tile in tiles:
-    print('http://a.tile.osm.org/%d/%d/%d.png' % tile)
-    time.sleep(1)
--- static/app.js
+++ static/app.js
@@ -5,7 +5,9 @@ WEB_SOCKET_DEBUG = true;
 var socket = io.connect('/track');
 
 var path;
+var pathHead;
 var map;
+var ourPathHead;
 
 $(window).bind("beforeunload", function() {
     socket.disconnect();
@@ -16,7 +18,8 @@ socket.on('connect', function () {
 });
 
 socket.on('position', function (latitude, longitude, altitude) {
-    path.addLatLng([latitude, longitude]);
+    pathHead.setLatLng([latitude, longitude]);
+    path.addLatLng(pathHead.getLatLng());
     map.fitBounds(path.getBounds());
 });
 
@@ -39,15 +42,30 @@ function message (from, msg) {
 
 $(document).ready(function() {
     map = L.map('map').setView([40.0245, -118.936], 13);
-    // L.tileLayer('http://localhost:5000/static/tiles/tile1.png', {
-        // maxZoom: 18
-    // }).addTo(map);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    L.tileLayer('http://localhost:5000/static/images/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-        maxZoom: 14
+        maxZoom: 18
     }).addTo(map);
+    // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        // attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        // maxZoom: 14
+    // }).addTo(map);
 
-    var marker = L.marker([40.0245, -118.936]).addTo(map);
+    ourPath = L.polyline([], {color: 'blue'}).addTo(map);
+    ourPathHead = L.marker([40.0245, -118.936]).addTo(map);
     // create a red polyline from an arrays of LatLng points
     path = L.polyline([], {color: 'red'}).addTo(map);
+    pathHead = L.circle([40.0245, -118.936], 100, {'color': 'red'}).addTo(map);
+
+    $("#our_latitude").val(ourPathHead.getLatLng().lat);
+    $("#our_longitude").val(ourPathHead.getLatLng().lng);
+    $("#our_location").submit(function(e) {
+        e.preventDefault();
+        ourPathHead.setLatLng([
+                parseFloat($("#our_latitude").val()),
+                parseFloat($("#our_longitude").val()),
+            ]);
+        ourPath.addLatLng(ourPathHead.getLatLng());
+        return false;
+    });
 });
